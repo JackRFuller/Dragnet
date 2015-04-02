3234b25342710423298b44c52e584788eddf4e58
JackRFuller <jackrfuller@outlook.com>
2015-04-02 15:25:01 +0100

    Basic Character Movement w/out Animation
    Basic Character Movement alongside camera behaviour

---
+++ Dragnet_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] float Speed;
+
+	public Vector3[] LookAtTargets;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+
+
+		GetComponent<Rigidbody> ().velocity = new Vector3 (x * Speed,0, z * Speed);
+		transform.rotation = Quaternion.LookRotation(new Vector3(x,0,z));
+
+		//LockRotation (x,z);
+
+	
+	}
+
+	void LockRotation(float X_Axis, float Z_Axist)
+	{
+		if(X_Axis > 0.1 && Z_Axist == 0)
+		{
+			transform.LookAt(LookAtTargets[0]);
+		}
+		if(X_Axis < -0.1 && Z_Axist == 0){
+		}
+		if(X_Axis > 0 && Z_Axist == 0.1){
+		}
+		if(X_Axis > 0 && Z_Axist == -0.1){
+		}
+		if(X_Axis > 0.1 && Z_Axist == 0.1){
+		}
+		if(X_Axis > -0.1 && Z_Axist == -0.1){
+		}
+	}
+}
